f92d8c7e73a438f2fcd72a762aeb37b9bf7a3c39
ab1975 <ajaykbhosle@gmail.com>
2020-01-13 16:53:59 +0530

    First Commit

---
+++ src/main/java/springbootdockerjenkins/SpringBootDockerJenkinsDemoApplication.java
@@ -0,0 +1,13 @@
+package springbootdockerjenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDockerJenkinsDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDockerJenkinsDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/springbootdockerjenkins/SpringBootDockerJenkinsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package springbootdockerjenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDockerJenkinsDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
